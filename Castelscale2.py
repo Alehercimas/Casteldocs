e13aa96aa467434b79bfa2333a8d1ffc39018626
Aleksy Herominski <61437293+Alehercimas@users.noreply.github.com>
2020-02-27 12:11:54 +0000

    Create Castelscale2.py

---
+++ Castelscale2.py
@@ -0,0 +1,25 @@
+# scaling an n*3 array (n lots of (x,y,z) coordinates) by whatever size needed
+
+import numpy as np
+
+def scale3m2(s, xx, yy, zz): # Remember to use the 
+    pp = np.hstack((s[:-1], s[1:]))
+    a, b = np.shape(s)
+    c = np.arange(a - 2)
+    t = np.linspace(0,1,11)
+    p = np.array([0,0,0])
+    q = np.array([0,0,0])
+    for h in t:
+        for i in c:
+            for j in pp:
+                p = np.append(p,(((1-h)*j[:-3]) + (h*j[3:]))).reshape(-1,3)
+            pp = np.hstack((p[1:-1], p[2:]))
+            p = np.array([0,0,0])
+        q = np.append(q, (((1-h)*pp[:,:-3]) + (h*pp[:,3:]))).reshape(-1,3)
+        pp = np.hstack((s[:-1], s[1:]))
+    q = q[1:]
+    a, b = np.shape(q)
+    a = np.arange(a)
+    for k in a:
+        q[k] = scale3m(q[k], xx, yy, zz)
+    return q
